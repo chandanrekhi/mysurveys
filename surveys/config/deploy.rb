81c46cd973463a0ea5625f9e88f68764b9cb0ebd
Chandan Rekhi <chandan@appcentre05s-MacBook-Pro.local>
2014-10-19 21:12:31 +0100

    Added remote cache

--- surveys/config/deploy.rb
+++ surveys/config/deploy.rb
@@ -21,6 +21,7 @@ default_run_options[:pty] = true
 set :user, 'ruby'
 set :use_sudo, 'false'
 set :scm, :git
+set :deploy_via, :remote_cache
 set :scm_passphrase, "unf0ld8"
 set :stages, %w(production staging preprod)
 
