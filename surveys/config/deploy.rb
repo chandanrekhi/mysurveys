d963dabb9098260edc84557bd183767cea83deb1
Chandan Rekhi <chandan@appcentre05s-MacBook-Pro.local>
2014-10-19 19:39:50 +0100

    changes made to deploy.rb

--- surveys/config/deploy.rb
+++ surveys/config/deploy.rb
@@ -3,7 +3,7 @@ require 'bundler/capistrano'
 require 'capistrano/ext/multistage'
 
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+#lock '3.2.1'
 
 set :application, 'Surveys'
 set :repo_url, 'git@github.com:chandanrekhi/mysurveys.git'
@@ -44,24 +44,24 @@ set :stages, %w(production staging preprod)
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+  #desc 'Restart application'
+  #task :restart do
+   # on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+    #end
+  #end
 
-  after :publishing, :restart
+ # after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #after :restart, :clear_cache do
+  #  on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-    end
-  end
+    #end
+  #end
 
 desc 'Start the Thin process'
   task :start, :roles => :app do
